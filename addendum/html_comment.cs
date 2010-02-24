6de157e57e21d9f8c4da5cdacd86e84b4ff723ea
Michal Čihař <mcihar@novell.com>
2010-02-24 14:43:50 +0100

    Add header pointing users to translation server to translated documents.

---
+++ addendum/html_comment.cs
@@ -0,0 +1,13 @@
+PO4A-HEADER:mode=before;position=<!-- \$Id
+
+<!--
+
+This file is generated using po4a, do not edit!
+
+You can edit po files to change the translation.
+
+Or you can edit them online at https://l10n.cihar.com/projects/pmadoc/.
+
+-->
+
+
