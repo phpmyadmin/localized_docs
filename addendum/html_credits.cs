ddda8dfd2857495cf2afcb52775a81159cc9e930
Michal Čihař <mcihar@novell.com>
2010-09-07 16:05:45 +0200

    Forgotten files for credits.

---
+++ addendum/html_credits.cs
@@ -0,0 +1,6 @@
+PO4A-HEADER:mode=before;position=<h2 id="glossary">
+<p>
+Tento překlad vznikl spoluprácí překladatelů na <a
+href="https://l10n.cihar.com/projects/pmadoc/">překladovém serveru</a>. Mimo
+jiné se na něm podíleli: Michal Čihař.
+</p>
