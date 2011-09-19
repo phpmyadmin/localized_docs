fea92e01f6a45d0cc0ee7f3b4a587fbdab40dfe8
Michal Čihař <mcihar@suse.cz>
2011-09-19 09:42:52 +0200

    Update translated docs

--- addendum/html_credits.pl
+++ addendum/html_credits.pl
@@ -3,4 +3,5 @@ PO4A-HEADER:mode=before;position=<h2 id="glossary">
 To tłumaczenie zostało Wam dostarczone przez różnych tłumaczy z serwera <a
 href="https://l10n.cihar.com/projects/pmadoc/">serwer
 tłumaczenia</a>. Właśnie tutaj powinni oni wpisać swoje imiona i nazwiska.
+Stanisław Krukowski
 </p>
