17687467949aa462fdec70372b5326573a37a090
Michal Čihař <michal@cihar.com>
2011-10-11 09:04:04 +0200

    Update generated files

--- addendum/html_head.pl
+++ addendum/html_head.pl
@@ -1,11 +1,10 @@
 PO4A-HEADER:mode=after;position=<div id="body">;beginboundary=<ul>
 <p>
-To jest (<b>częściowe</b>) tłumaczenie oficjalnej <a
+  To jest (<b>częściowe</b>) tłumaczenie oficjalnej <a
 href="http://www.phpmyadmin.net /documentation/">dokumentacji programu
-phpMyAdmin -- wersja 3.5.0-dev</a>.
-
-Wszystkich sympatyków, zwolenników i użytkowników <b>Wolnego
-Oprogramowania</b> zapraszamy do wspólnego tłumaczenia tej
-dokumentacji. Nasz serwer tłumaczeń znajduje sie pod adresem: <a
+phpMyAdmin -- wersja 3.5.0-dev</a>.Wszystkich sympatyków, zwolenników i
+użytkowników <b>Wolnego Oprogramowania</b> zapraszamy do wspólnego
+tłumaczenia tej dokumentacji. Nasz serwer tłumaczeń znajduje sie pod
+adresem: <a
 href="https://l10n.cihar.com/projects/pmadoc/">https://l10n.cihar.com/projects/pmadoc/</a>
 </p>
