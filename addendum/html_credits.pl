94536c2813a34ac731c330671f1db783dfca44bf
Michal Čihař <michal@cihar.com>
2011-10-13 11:49:12 +0200

    Update generated files

--- addendum/html_credits.pl
+++ addendum/html_credits.pl
@@ -1,9 +1,9 @@
 PO4A-HEADER:mode=before;position=<h2 id="glossary">
 <p>
-  To tłumaczenie jest <b>wspólną</b> pracą <b>różnych</b> tłumaczy, którzy
+To tłumaczenie jest <b>wspólną</b> pracą <b>różnych</b> tłumaczy, którzy
 pracowali nad tłumaczeniem, z języka angielskiego, dokumentacji phpMyAdmin
 -- wersja 3.5.0-dev, na <a
 href="https://l10n.cihar.com/projects/pmadoc/">serwerze tłumaczeń
 (Pootle)</a>. Właśnie tutaj powinni oni podpisać się (w dowolnej
-kolejności):< /br>-- Stanisław Krukowski --
+kolejności):-- Stanisław Krukowski --
 </p>
--- addendum/html_head.pl
+++ addendum/html_head.pl
@@ -1,6 +1,6 @@
 PO4A-HEADER:mode=after;position=<div id="body">;beginboundary=<ul>
 <p>
-  To jest (<b>częściowe</b>) tłumaczenie oficjalnej <a
+To jest (<b>częściowe</b>) tłumaczenie oficjalnej <a
 href="http://www.phpmyadmin.net /documentation/">dokumentacji programu
 phpMyAdmin -- wersja 3.5.0-dev</a>.Wszystkich sympatyków, zwolenników i
 użytkowników <b>Wolnego Oprogramowania</b> zapraszamy do wspólnego
