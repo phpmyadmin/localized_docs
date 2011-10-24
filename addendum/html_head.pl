c72c720f1fdeb887fbf9b755ec83c8e7312d1831
Michal Čihař <mcihar@suse.cz>
2011-10-24 12:41:58 +0200

    Update generated docs

--- addendum/html_head.pl
+++ addendum/html_head.pl
@@ -2,7 +2,7 @@ PO4A-HEADER:mode=after;position=<div id="body">;beginboundary=<ul>
 <p>
 To jest (<b>częściowe</b>) tłumaczenie oficjalnej <a
 href="http://www.phpmyadmin.net /documentation/">dokumentacji programu
-phpMyAdmin -- wersja 3.5.0-dev</a>.Wszystkich sympatyków, zwolenników i
+phpMyAdmin -- wersja 3.5.0-dev</a>. Wszystkich sympatyków, zwolenników i
 użytkowników <b>Wolnego Oprogramowania</b> zapraszamy do wspólnego
 tłumaczenia tej dokumentacji. Nasz serwer tłumaczeń znajduje sie pod
 adresem: <a
