8f6f5babc20e0523d5fcaba9055103e8c140782d
Michal Čihař <mcihar@suse.cz>
2012-01-19 15:30:14 +0100

    Update generated docs

--- addendum/html_credits.pl
+++ addendum/html_credits.pl
@@ -4,6 +4,5 @@ To tłumaczenie jest <b>wspólną</b> pracą <b>różnych</b> tłumaczy, którzy
 pracowali nad tłumaczeniem, z języka angielskiego, dokumentacji phpMyAdmin
 -- wersja 3.5.0-dev, na <a
 href="https://l10n.cihar.com/projects/pmadoc/">serwerze tłumaczeń
-(Pootle)</a>. Właśnie tutaj powinni oni podpisać się (w dowolnej
-kolejności):-- Stanisław Krukowski --
+(Pootle)</a>. Właśnie tutaj powinni się oni podpisać: - Stanisław Krukowski,
 </p>
