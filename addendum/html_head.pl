7f6a366a144ffbc03c6b991e199866f967d5fedb
Michal Čihař <mcihar@suse.cz>
2012-04-19 10:56:25 +0200

    Update generated docs

--- addendum/html_head.pl
+++ addendum/html_head.pl
@@ -5,5 +5,5 @@ href="http://www.phpmyadmin.net /documentation/">dokumentacji programu
 phpMyAdmin - wersja 3.5.0-beta1-dev</a>. Wszystkich użytkowników phpMyAdmin
 zapraszamy do wspólnego tłumaczenia tej dokumentacji. Nasz serwer tłumaczeń
 znajduje sie pod adresem: <a
-href="https://l10n.cihar.com/projects/pmadoc/">https://l10n.cihar.com/projects/pmadoc/</a>
+href="https://l10n.cihar.com/projects/pmadoc/">https://l10n.cihar.com/projects/pmadoc/</a>.
 </p>
