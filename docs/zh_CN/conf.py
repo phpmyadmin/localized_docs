b4486a90af165a7f53f3a305c382d2db2338c70a
Michal Čihař <michal@cihar.com>
2017-12-16 18:40:37 +0100

    Add Russian
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- docs/zh_CN/conf.py
+++ docs/zh_CN/conf.py
@@ -308,7 +308,3 @@ lexers['php'] = PhpLexer(startinline=True)
 linkcheck_retries = 10
 linkcheck_timeout = 10
 linkcheck_anchors = False
-linkcheck_ignore = [
-    # Site is often down
-    r'https://software.opensuse.org/package/.*',
-]
